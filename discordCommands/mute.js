be37348d5f510bc96d92bd3c66496ab01e8d1531
Adam Cooper <frankidyfrank3@gmail.com>
2019-09-30 09:52:54 -0500

    mute worksgit add .git add . wooo!

--- discordCommands/mute.js
+++ discordCommands/mute.js
@@ -1,5 +1,6 @@
 let Discord = require("discord.js");
 let getUser = require("../discordFunctions/findUser");
+let getMuteRole = require("../discordFunctions/makeMuteRole").getMuteRole;
 
 module.exports = {
   help: {
@@ -10,24 +11,32 @@ module.exports = {
     if (args[0] === undefined)
       return message.channel.send("ERROR: no user given");
     const actionUser = await getUser(bot, message, args[0]);
+    let muteRole = await getMuteRole(bot, message);
+    let muted = false;
+    await actionUser.roles.forEach(role => {
+      if (role.id === muteRole.id) muted = true;
+    });
+    let action = "mute";
+    if (muted) action = "unmute";
     args.splice(0, 1);
     db.Infractions.create({
       userID: actionUser.id,
       staffID: message.author.id,
-      type: "mute",
+      type: action,
       channelName: message.channel.name,
       message: args.join(" ") || ""
     });
     const embed = new Discord.RichEmbed()
       .addField("User", actionUser.user.username)
       .addField("Staff", message.author.username)
-      .addField("type", "mute")
-      .addField("message", args.join(" ") || " ");
+      .addField("type", action)
+      .addField("message", args.join(" ") || "No Reason Given");
     message.channel.send(embed);
     if (serverProfile.modLogs !== "")
       message.guild.channels
         .find(chan => chan.id === serverProfile.modLogs)
         .send(embed);
-    message.guild.member(actionUser);
+    if (!muted) return await actionUser.addRole(muteRole.id);
+    await actionUser.removeRole(muteRole.id);
   }
 };
