0dc1f3610cade4fffb6423b06e54abcf4104c250
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-30 11:47:01 -0500

    added logs & infractions route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,4 +23,38 @@ module.exports = function(app) {
       res.json(dbExample);
     });
   });
+
+  app.get("/api/infractions", function(req, res) {
+    if (!req.session.user) {
+      return res.status(401).send({
+        status: "ERROR",
+        error: "Unauthorized"
+      });
+    }
+    db.Infractions.findAll({
+      where: {
+        authorId: req.body.id
+      }
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/logs", function(req, res) {
+    if (!req.session.user) {
+      return res.status(401).send({
+        status: "ERROR",
+        error: "Unauthorized"
+      });
+    }
+    db.serverLogs
+      .findAll({
+        where: {
+          authorLogs: req.body.id
+        }
+      })
+      .then(function(results) {
+        res.json(results);
+      });
+  });
 };
